344f922ceeea4c0bd78776ba1d6f762575dabdd4
MizterB <5458030+MizterB@users.noreply.github.com>
2019-01-23 17:17:09 -0500

    Conforming to card-tools 0.3 developer instructions

--- secondaryinfo-entity-row.js
+++ secondaryinfo-entity-row.js
@@ -1,50 +1,53 @@
-var LitElement = LitElement || Object.getPrototypeOf(customElements.get('hui-error-entity-row'));
+customElements.whenDefined('card-tools').then(() => {
 
-class SecondaryInfoEntityRow extends LitElement {
+    class SecondaryInfoEntityRow extends cardTools.litElement() {
+        render() {
+            return cardTools.litHtml()`
+                ${this._wrappedElement}
+            `;
+        }
 
-  render() {
-    return cardTools.litHtml()`
-        ${this._wrappedElement}
-    `;
-  }
+        setConfig(config) {
+            cardTools.checkVersion(0.3);
+            this._config = config;
+            this._wrappedElement = this._createElement(config);
+            this.requestUpdate();
+        }
 
-  firstUpdated() {
-    this._updateElement(this._wrappedElement, this._hass);
-  }
+        set hass(hass) {
+            this._hass = hass;
+            this._stateObj = this._config.entity in hass.states ? hass.states[this._config.entity] : null;
+            this._updateElement(this._wrappedElement, hass);
+        }
 
-  setConfig(config) {
-    if(!window.cardTools) throw new Error(`Can't find card-tools. See https://github.com/thomasloven/lovelace-card-tools`);
-    window.cardTools.checkVersion(0.3);
-    this._config = config;
-    this._wrappedElement = this._createElement(config);
-    this.requestUpdate();
-  }
+        _createElement(config) {
+            // Override the custom row type in order to create the 'standard' row for this entity
+            let defaultRowConfig = Object.assign(config, {type: "default"});
+            const element = cardTools.createEntityRow(defaultRowConfig);
+            return element;
+        }
 
-  set hass(hass) {
-    this._hass = hass;
-    this._stateObj = this._config.entity in hass.states ? hass.states[this._config.entity] : null;
-    this._updateElement(this._wrappedElement, hass);
-  }
+        async _updateElement(wrappedElement, hass) {
+            if (!wrappedElement) return;
 
-  _createElement(config) {
-    // Override the custom row type in order to create the 'standard' row for this entity
-    let defaultRowConfig = Object.assign(config, {type: "default"});
-    const element = window.cardTools.createEntityRow(defaultRowConfig);
-    return element;
-  }
-
-  async _updateElement(wrappedElement, hass) {
-    if (!wrappedElement) return;
-
-    this._wrappedElement.hass = hass;
-    await this._wrappedElement.updateComplete;
-    let secondaryInfoDiv = this._wrappedElement.shadowRoot.querySelector("hui-generic-entity-row").shadowRoot.querySelector(".secondary");
-    if (secondaryInfoDiv && this._config.secondary_info) {
-      let text = window.cardTools.parseTemplate(this._config.secondary_info, 'Unable to parse secondary_info config');
-      secondaryInfoDiv.innerHTML = text;
+            this._wrappedElement.hass = hass;
+            await this._wrappedElement.updateComplete;
+            let secondaryInfoDiv = this._wrappedElement.shadowRoot.querySelector("hui-generic-entity-row").shadowRoot.querySelector(".secondary");
+            if (secondaryInfoDiv && this._config.secondary_info) {
+                let text = window.cardTools.parseTemplate(this._config.secondary_info, 'Unable to parse secondary_info config');
+                secondaryInfoDiv.innerHTML = text;
+            }
+        }
     }
-  }
+    customElements.define('secondaryinfo-entity-row', SecondaryInfoEntityRow);
 
-}
+});
 
-customElements.define('secondaryinfo-entity-row', SecondaryInfoEntityRow);
+setTimeout(() => {
+    if (customElements.get('card-tools')) return;
+    customElements.define('secondaryinfo-entity-row', class extends HTMLElement {
+        setConfig() {
+            throw new Error("Can't find card-tools. See https://github.com/thomasloven/lovelace-card-tools");
+        }
+    });
+}, 2000);
