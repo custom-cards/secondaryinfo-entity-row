aa2838a20b4a1c48eb6a3c40bf960eb46eeba771
Ian Richardson <iantrich@gmail.com>
2021-06-05 16:12:27 -0500

    Revert "remove card-tools requirement"

--- secondaryinfo-entity-row.js
+++ secondaryinfo-entity-row.js
@@ -1,34 +1,10 @@
-console.info(
-  `%c SECONDARYINFO-ENTITY-ROW  \n%c  Version 0.6  `,
-  "color: orange; font-weight: bold; background: black",
-  "color: white; font-weight: bold; background: dimgray",
- );
+customElements.whenDefined('card-tools').then(() => {
 
-var Lit = Lit || Object.getPrototypeOf(customElements.get("ha-panel-lovelace") || customElements.get('hui-view'));
-var html = Lit.prototype.html;
-
-const helpers = await window.loadCardHelpers();
-
-export async function parseTemplate(hass, str, specialData = {}) {
-  if (typeof(specialData === "string")) specialData = {};
-    specialData = Object.assign({
-      user: hass.user.name,
-      hash: location.hash.substr(1) || ' ',
-    },
-    specialData);
-
-    for (var k in specialData) {
-      var re = new RegExp(`\\{${k}\\}`, "g");
-      str = str.replace(re, specialData[k]);
-    }
-
-    return hass.callApi("POST", "template", {template: str});
-};
-
-    class SecondaryInfoEntityRow extends Lit {
+    class SecondaryInfoEntityRow extends cardTools.LitElement {
+        version() { return "0.5"; }
 
         render() {
-            return html`
+            return cardTools.LitHtml`
                 ${this._wrappedElement}
             `;
         }
@@ -50,7 +26,7 @@ export async function parseTemplate(hass, str, specialData = {}) {
             // Override the custom row type in order to create the 'standard' row for this entity
             let defaultRowConfig = Object.assign({}, config);
             delete defaultRowConfig.type;
-            const element = helpers.createRowElement(defaultRowConfig);
+            const element = cardTools.createEntityRow(defaultRowConfig);
             return element;
         }
 
@@ -64,12 +40,23 @@ export async function parseTemplate(hass, str, specialData = {}) {
             if (secondaryInfoDiv && this._config.secondary_info) {
                 let text;
                 if (this._config.secondary_info.includes('{{') || this._config.secondary_info.includes('{%')) {
-                    text = await parseTemplate(hass, this._config.secondary_info, {entity: this._config.entity})
+                    text = await window.cardTools.parseTemplate(hass, this._config.secondary_info, {entity: this._config.entity})
                 } else {
-                    text = parseTemplate(this._config.secondary_info, {entity: this._config.entity});
+                    text = window.cardTools.parseTemplate(this._config.secondary_info, {entity: this._config.entity});
                 }
                 secondaryInfoDiv.innerHTML = text;
             }
         }
     }
-    customElements.define('secondaryinfo-entity-row', SecondaryInfoEntityRowCard);
+    customElements.define('secondaryinfo-entity-row', SecondaryInfoEntityRow);
+
+});
+
+setTimeout(() => {
+    if (customElements.get('card-tools')) return;
+    customElements.define('secondaryinfo-entity-row', class extends HTMLElement {
+        setConfig() {
+            throw new Error("Can't find card-tools. See https://github.com/thomasloven/lovelace-card-tools");
+        }
+    });
+}, 2000);
